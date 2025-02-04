344741cc632f5c333cbe4711167dd6494c455cd5
David Behar <davidbehar@Davids-MacBook-Pro-2.local>
2025-02-03 21:49:55 -0600

    Remove unused code

--- src/index.cjs
+++ src/index.cjs
@@ -7,22 +7,7 @@ const apiKey = process.env.API_KEY;
 
 const TEMPLATE_JSON = fs.readFileSync(__dirname + "/templates/template2.json", "utf8");
 
-function getFinalTemplate(params) {
-  let template = TEMPLATE_JSON;
-  for (const [key, value] of Object.entries(params)) {
-    const regex = new RegExp(`{{${key}}}`, "g");
-    template = template.replace(regex, value);
-  }
-  return JSON.parse(template);
-}
-
-const params = {
-  backgroundImage: "https://via.assets.so/img.jpg?w=1080&h=1920&tc=blue&bg=#cecece",
-  questionText: "¿Cuál es la capital de Japón?",
-  finalMessage: "¡Correcto! La respuesta es Tokio.",
-};
-
-const finalTemplate = getFinalTemplate(params);
+const finalTemplate = JSON.parse(TEMPLATE_JSON);
 
 const client = new Client(apiKey);
 
@@ -30,7 +15,7 @@ const client = new Client(apiKey);
   const renders = await client.render({
     source: finalTemplate,
     modifications: {
-      // "Background": "https://miservidor.com/nueva-imagen.jpg",
+      "Background": "https://via.assets.so/img.jpg?w=1080&h=1920&tc=blue&bg=#cecece",
       "Question-Text": "¿Cuál es la capital de Japón?",
       "OptionA-Text": "A) Seúl",
       "OptionB-Text": "B) Pekín",
